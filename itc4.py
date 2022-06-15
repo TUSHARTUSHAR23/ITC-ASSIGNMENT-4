98191981daeae217f25a8aa7484b0b9a59cdfcbe
TUSHARTUSHAR23 <107211946+TUSHARTUSHAR23@users.noreply.github.com>
2022-06-14 21:44:40 -0700

    Add files via upload

---
+++ itc4.py
@@ -0,0 +1,25 @@
+Enter Marks :45
+ Grade D 
+Enter a year: 2004
+year is a Leap Year
+Question 1:3X10=6
+Wrong.The right answer is 30
+Question 2:9X8=72
+Right!
+Question 3:4X6=24
+Right!
+Question 4:4X6=24
+Right!
+Question 5:8X3=18
+Wrong.The right answer is 24
+Question 6:2X1=2
+Right!
+Question 7:7X2=14
+Right!
+Question 8:5X3=15
+Right!
+Question 9:10X6=60
+Right!
+Question 10:1X3=3
+Right!
+177 candies are in the bowl!
